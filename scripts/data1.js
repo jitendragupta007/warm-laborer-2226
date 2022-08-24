4ac7bdb95846ed6e112e892a398ec66e94f2f618
jitendragupta007 <itsjitendragupta007@gmail.com>
2022-08-24 23:41:37 +0530

    added new content

---
+++ scripts/data1.js
@@ -0,0 +1,58 @@
+category_data=[
+{
+img='https://dao54xqhg9jfa.cloudfront.net/OMS-Category/…6148-d407-95522fa17a28/original/Todays-deal21.png',
+category="Today's Deals",
+},
+
+{
+img:'https://dao54xqhg9jfa.cloudfront.net/OMS-Category/479a9750-d9a2-33ff-0693-3044a9dcae91/original/Chicken.png',
+category:'Chicken',
+},
+
+
+{
+    img:,
+    category:,
+    },
+
+    {
+        img:,
+        category:,
+        },
+        {
+            img:,
+            category:,
+            },
+            {
+                img:,
+                category:,
+                },
+                {
+                    img:,
+                    category:,
+                    },
+                    {
+                        img:,
+                        category:,
+                        },
+                        {
+                            img:,
+                            category:,
+                            },
+                            {
+                                img:,
+                                category:,
+                                },
+
+                                {
+                                    img:,
+                                    category:,
+                                    },
+                                    {
+                                        img:,
+                                        category:,
+                                        },
+
+
+
+]
