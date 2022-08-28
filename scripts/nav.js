3ca98ac265e432bf0c56dc192629c9dc7cba77dc
jitendragupta007 <itsjitendragupta007@gmail.com>
2022-08-28 17:09:54 +0530

    added new content

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,10 @@
 let nav=`
-<a href="#" ><img id="nav-img" src="https://www.licious.in/img/rebranding/licious-logo.svg" alt="logo"/></a>
+<a href="index.html" ><img id="nav-img" src="https://www.licious.in/img/rebranding/licious-logo.svg" alt="logo"/></a>
 <input type=" text" id="location" Placeholder=" Your Location"/>
 <input type="search" id="search" placeholder="Search for any delicious Product"/>
 <a href="#" id="category"><img src="https://www.licious.in/img/rebranding/category-dropdown-icon.svg" alt="Nav"/> Categories</a>
-<a href="#" id="login"><img src="https://www.licious.in/img/rebranding/profile_icon.svg" alt="Login"/> Login</a>   
-<a href="#" id="cart"><img src="https://www.licious.in/img/rebranding/cart_icon.svg" alt="Category"/> Cart</a>`;
+<a href="signin.html" id="login"><img src="https://www.licious.in/img/rebranding/profile_icon.svg" alt="Login"/> Login</a>   
+<a href="cart.html" id="cart"><img src="https://www.licious.in/img/rebranding/cart_icon.svg" alt="Category"/> Cart</a>`;
 
 
 
